6e7f84c68d6607dcace362525fd7bec5d5437663
Drew Williams <WilliamsAg2@hendrix.edu>
2026-02-16 17:04:36 -0600

    Current progress...

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
-
-function greenLight(){
-            $(".go").css("opacity", "100%")
-
-            $(".yield").css("opacity", "20%")
-
-            $(".stop").css("opacity", "20%")
-
-}
-function yellowLight() {
-            $(".go").css("opacity", "20%")
-
-            $(".yield").css("opacity", "100%")
-
-            $(".stop").css("opacity", "20%")
-}
-function redLight() {
-            $(".go").css("opacity", "20%")
-
-            $(".yield").css("opacity", "20%")
-
-            $(".stop").css("opacity", "100%")
-}
-var next = greenLight;
-
-var stopLight = function() {
-    if (next == greenLight) {
-        next = redLight;
-    } else if(next == redLight){
-        next = yellowLight;
-    } else {
-        next = greenLight;
-    }
-    next();
+function newJoke(){
+    $.ajax({
+      dataType: "json",
+      url: "https://geek-jokes.sameerkumar.website/api?format=json",
+      success: function(results) {
+        if (results["url"].endsWith(".mp4")) {
+          $('#start').attr("str", "Whats up!");
+        } else {
+          $('#jokes').text("didn't work");
+        }
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
 }
 
-var clickButton = $("#clicker");
-clickButton.click(stopLight);
-console.log("loaded");
-console.log(clickButton);
 
+
+
+function newEntree(){
+    $.ajax({
+      dataType: "json",
+      url: " https://foodish-api.com/images/pizza/",
+      success: function(results) {
+        if (results["url"].endsWith(".mp4")) {
+          $('#finish').attr("src", "https://tse1.mm.bing.net/th/id/OIP.3f4uw03GjHN2wa2tSeNc4wHaIu?pid=Api&P=0&h=220");
+        } else {
+          $('#finish').attr("src", results["url"]);
+        }
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+}
+
+function newAppetizer(){
+    $.ajax({
+      dataType: "json",
+      url: " https://foodish-api.com/images/pizza/",
+      success: function(results) {
+        if (results["url"].endsWith(".mp4")) {
+          $('#app').attr("src", "images/blank.png");
+        } else {
+          $('#app').attr("src", results["url"]);
+        }
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+}
+
+var entreeButton = $("#ent");
+entreeButton.click(newEntree);
+
+var appetizerButton = $("#app");
+appetizerButton.click(newAppetizer);
+
+var jokeButton = $("#joke");
+jokeButton.click(newJoke);
