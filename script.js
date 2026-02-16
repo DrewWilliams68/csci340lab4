c35f407ef1f58e45293ea9bd322d431cd3cdce99
Drew Williams <WilliamsAg2@hendrix.edu>
2026-02-16 14:08:27 -0600

    Altered chess format test

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-hi
+
+function greenLight(){
+            $(".go").css("opacity", "100%")
+
+            $(".yield").css("opacity", "20%")
+
+            $(".stop").css("opacity", "20%")
+
+}
+function yellowLight() {
+            $(".go").css("opacity", "20%")
+
+            $(".yield").css("opacity", "100%")
+
+            $(".stop").css("opacity", "20%")
+}
+function redLight() {
+            $(".go").css("opacity", "20%")
+
+            $(".yield").css("opacity", "20%")
+
+            $(".stop").css("opacity", "100%")
+}
+var next = greenLight;
+
+var stopLight = function() {
+    if (next == greenLight) {
+        next = redLight;
+    } else if(next == redLight){
+        next = yellowLight;
+    } else {
+        next = greenLight;
+    }
+    next();
+}
+
+var clickButton = $("#clicker");
+clickButton.click(stopLight);
+console.log("loaded");
+console.log(clickButton);
+
